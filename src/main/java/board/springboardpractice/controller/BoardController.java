ed768b5b105e30961421920c681953ea631617cc
Hyeri1ee <haerizian10@gmail.com>
2024-12-30 17:38:40 +0900

    fix: hasAuthority로 해결

--- src/main/java/board/springboardpractice/controller/BoardController.java
+++ src/main/java/board/springboardpractice/controller/BoardController.java
@@ -1,9 +1,12 @@
 package board.springboardpractice.controller;
 
+import board.springboardpractice.jwt.SecurityUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 @ResponseBody
 public class TestController {
@@ -16,6 +19,9 @@ public class TestController {
 
   @GetMapping("/bronze/board")
   public String bronzeBoard() {
+
+    String loginId = SecurityUtil.getCurrentUserLoginId();
+    log.info("현재 /bronze/board에 로그인한 사용자 : {}" , loginId);
 
     return "bronzeBoard accessed!";
   }
